a577224685eeffd768c2c0b479738694f23a1973
sompadidvirada <sompadidppp@gmail.com>
2025-07-23 16:09:16 +0700

    change server file

--- foodbank-server/server.js
+++ foodbank-server/server.js
@@ -6,10 +6,10 @@ const app = express();
 
 const server = http.createServer(app); // create raw server for socket.io
 const morgan = require("morgan");
-const path = require("path");
-const fs = require("fs");
 const { readdirSync } = require("fs");
 const cors = require("cors");
+app.use(express.json({ limit: `100mb` }));
+app.use(morgan("dev"));
 
 const io = new Server(server, {
   cors: {
@@ -17,53 +17,24 @@ const io = new Server(server, {
     methods: ["GET", "POST"]
   }
 });
-
-app.use(express.json({ limit: `100mb` }));
-app.use(morgan("dev"));
-
 app.use(
   cors({
     origin: true, // Allow any origin (or specify yours)
     credentials: true,
   })
 );
-
 io.on("connect", (socket) => {
   console.log("A user connected:", socket.id);
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
   });
-
 });
-
 app.options("*", cors()); // handle preflight
-
-
-const defaultImage = "public/uploads/alexandar.png";
-
-app.get("/uploads/:imageName", (req, res) => {
-  const { imageName } = req.params;
-  const imagePath = path.join(__dirname, "public/uploads", imageName);
-
-  const sendCachedFile = (filePath) => {
-    res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
-    res.removeHeader("Last-Modified");
-    res.removeHeader("ETag");
-    res.sendFile(filePath, { headers: { "Cache-Control": "public, max-age=31536000, immutable" } });
-  };
-
-  if (fs.existsSync(imagePath)) {
-    sendCachedFile(imagePath);
-  } else {
-    sendCachedFile(path.join(__dirname, defaultImage));
-  }
-});
 
 readdirSync("./routes").map((item) =>
   app.use("/", require("./routes/" + item))
 );
-
 
 // Start server
 server.listen(3500, "0.0.0.0", () => {
